1cebc4dec3136aed9a798f9937f6a4a58362ea1c
ellectronchik <electronchik04@mail.ru>
2024-12-23 13:13:37 +0300

    Add admin user creation and trust token management queries to database initialization
    - Added new queries for creating and managing admin user and trust token in the database initialization process.
    - Ensured that the ADMIN_ID and ADMIN_TRUST_TOKEN_HASH environment variables are set before running the new queries.
    - Added error handling for missing environment variables and threw an error if they are not set.
    - Logged information messages for creating admin user and trust token in the database.

--- src/core/db/initialize.ts
+++ src/core/db/initialize.ts
@@ -2,6 +2,7 @@ import { Logger } from "bs-logger";
 import { Database } from "sqlite3";
 import dbCreatingQueries from "./queries/dbCreatingQueries";
 import dbIndexQueries from "./queries/dbIndexQueries";
+import dbAdminQueries from "./queries/dbAdminQueries";
 import loggerMessages from "assets/messages/loggerMessages.json";
 import formatMessage from "@/shared/lib/messages/formatMessage";
 
@@ -26,6 +27,31 @@ export default function initializeDb(db: Database, logger: Logger) {
       });
 
       logger.info(loggerMessages.info.database.indexes_created);
+
+      if (!process.env.ADMIN_ID) {
+        logger.error(loggerMessages.error.database.admin.id_not_defined);
+        throw new Error(loggerMessages.error.database.admin.id_not_defined);
+      }
+
+      if (!process.env.ADMIN_TRUST_TOKEN_HASH) {
+        logger.error(
+          loggerMessages.error.database.admin.trust_token_not_defined
+        );
+        throw new Error(
+          loggerMessages.error.database.admin.trust_token_not_defined
+        );
+      }
+
+      db.run(dbAdminQueries.createAdminUser, [process.env.ADMIN_ID]);
+
+      logger.info(loggerMessages.info.database.admin.user_created);
+
+      db.run(dbAdminQueries.createAdminTrustToken, [
+        process.env.ADMIN_ID,
+        process.env.ADMIN_TRUST_TOKEN_HASH,
+      ]);
+
+      logger.info(loggerMessages.info.database.admin.trust_token_created);
     });
 
     db.on("trace", (query) => {
