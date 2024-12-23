1f8a5ecbd959a3179803e6b130961488c5eb34ef
ellectronchik <electronchik04@mail.ru>
2024-12-23 13:29:39 +0300

    Add bot commands and enhance logger functionality
    - Added dotenv configuration to load environment variables from a .env file.
    - Updated logger setup to log messages from the bot and database operations.
    - Added backupJobManager to handle database backups.
    - Updated the bot command handler to handle the /start command.
    - Enhanced process event setup to include bot, error stream, info stream, and logger.
    
    These changes improve the logging functionality, add backup capabilities, and enhance the bot command handling.

--- src/core/bootstrap/entry.ts
+++ src/core/bootstrap/entry.ts
@@ -1,16 +1,30 @@
 import { Bot } from "grammy";
 import setupLogger from "@/features/logger/setup";
 import setupProcessEvents from "../helpers/setupProcessEvents";
+import path from "path";
+import loggerMessages from "assets/messages/loggerMessages.json";
+import chatMessages from "assets/messages/chatMessages.json";
+import dotenv from "dotenv";
+import backupJobManager from "../db/backup/backupJobManager";
+import ensureDatabase from "../db/ensure/ensureDatabase";
 
 export default async function entry() {
+  dotenv.config();
+
   const token = process.env.BOT_TOKEN;
 
-  const logger = await setupLogger();
+  const { logger, errorStream, infoStream } = await setupLogger();
+
+  const databasePath = path.resolve(__dirname, "data");
 
   if (!token) {
-    logger.error("BOT_TOKEN is not defined");
-    throw new Error("BOT_TOKEN is not defined");
+    logger.error(loggerMessages.error.bot.token);
+    throw new Error(loggerMessages.error.bot.token);
   }
+
+  await ensureDatabase(databasePath, logger);
+
+  backupJobManager.start(logger, databasePath);
 
   const bot = new Bot(token);
 
@@ -19,19 +33,12 @@ export default async function entry() {
     throw err;
   });
 
-  bot.on("message:text", (ctx) => {
-    logger.info(
-      `Message by ${ctx.message.from.first_name} ${
-        ctx.message.from.last_name || ""
-      }(id: ${ctx.message.from.id}): ${ctx.message.text} ${
-        ctx.message.forward_origin ? "forwarded" : ""
-      }`
-    );
-    ctx.reply("Echo: " + ctx.message.text);
+  bot.start();
+  logger.info(loggerMessages.info.bot.started);
+
+  bot.command("start", async (ctx) => {
+    await ctx.reply(chatMessages.welcome_message_html, { parse_mode: "HTML" });
   });
 
-  bot.start();
-  logger.info("Bot successfully started");
-
-  setupProcessEvents(logger);
+  setupProcessEvents({ bot, errorStream, infoStream, logger });
 }
