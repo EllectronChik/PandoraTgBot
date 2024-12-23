3701427d048d84031623411a1c3c85e8ef3fa433
ellectronchik <electronchik04@mail.ru>
2024-12-23 15:39:15 +0300

    Add error message formatting for stream errors
    - Introduced a new function `formatMessage` to format error messages.
    - Replaced hardcoded error messages with formatted messages using `formatMessage` in the `streamErrorLogger` function.
    - Updated the `errorStream.on("error", streamErrorLogger)` and `infoStream.on("error", streamErrorLogger)` event handlers to use the new `formatMessage` function.
    
    This change enhances the readability and consistency of error messages in the logger setup process.

--- src/features/logger/setup.ts
+++ src/features/logger/setup.ts
@@ -1,9 +1,11 @@
 import ensureDirs from "@/shared/lib/fs/ensureDirs";
 import purgeEmptyLog from "@/shared/lib/logger/purgeEmptyLog";
 import purgeOutdatedLog from "@/shared/lib/logger/purgeOutdatedLog";
+import formatMessage from "@/shared/lib/messages/formatMessage";
 import { createLogger, logLevelNameFor, LogLevels } from "bs-logger";
 import { createWriteStream } from "fs";
 import path from "path";
+import loggerMessages from "assets/messages/loggerMessages.json";
 
 export default async function setupLogger() {
   const errorLogPath = path.resolve(__dirname, "logs/errors");
@@ -69,10 +71,14 @@ export default async function setupLogger() {
     ],
   });
 
-  errorStream.on("error", (err) =>
-    logger.error(`Stream error: ${err.message}`)
-  );
-  infoStream.on("error", (err) => logger.error(`Stream error: ${err.message}`));
+  const streamErrorLogger = (err: Error) =>
+    logger.error(
+      formatMessage(loggerMessages.error.stream, { message: err.message })
+    );
+
+  errorStream.on("error", streamErrorLogger);
+
+  infoStream.on("error", streamErrorLogger);
 
   return { logger, errorStream, infoStream };
 }
