43c466aba86db2eb7d76cef6e86f108eb5253a74
ellectronchik <electronchik04@mail.ru>
2024-12-23 13:31:03 +0300

    Add backup functionality to database management
    - Implemented backup functionality for databases by creating a backup directory and copying the database file to the backup path.
    - Ensured the backup directory is created if it doesn't exist.
    - Logged informative messages to the console for better understanding of the backup process.
    - Improved the overall database management system by adding a backup feature.

---
+++ src/core/db/backup/backupDatabase.ts
@@ -0,0 +1,41 @@
+import formatMessage from "@/shared/lib/messages/formatMessage";
+import isValidDirectoryPath from "@/shared/lib/validators/isValidDirectoryPath";
+import { cp, mkdir } from "fs/promises";
+import path from "path";
+import loggerMessages from "assets/messages/loggerMessages.json";
+import { Logger } from "bs-logger";
+
+export default async function backupDatabase(
+  logger: Logger,
+  databasePath: string
+) {
+  logger.info("Backup database called");
+  const backupPath = path.join(databasePath, "backups");
+  try {
+    if (!(await isValidDirectoryPath(backupPath))) {
+      await mkdir(backupPath);
+    }
+
+    logger.info(
+      formatMessage(loggerMessages.info.database.backup.creating, {
+        backupPath,
+      })
+    );
+    await cp(
+      path.join(databasePath, "database.sqlite"),
+      path.join(backupPath, `database_backup_${Date.now()}` + ".sqlite")
+    );
+
+    logger.info(
+      formatMessage(loggerMessages.info.database.backup.created, {
+        backupPath,
+      })
+    );
+  } catch {
+    logger.error(
+      formatMessage(loggerMessages.error.database.backup.creating, {
+        backupPath,
+      })
+    );
+  }
+}
