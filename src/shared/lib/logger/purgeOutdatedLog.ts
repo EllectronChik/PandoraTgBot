c8dc763c9aacc62305f736eb25499ddd1bd76500
ellectronchik <electronchik04@mail.ru>
2024-12-23 15:54:49 +0300

    Add logging messages for file operations
    - Added a call to `formatMessage` to log messages in the `purgeOutdatedLog` function.
    - Replaced hardcoded error messages with formatted messages from the `loggerMessages.json` file.
    - Updated logging messages for directory and file operations.
    - Enhanced the logging functionality to provide more informative and structured messages.

--- src/shared/lib/logger/purgeOutdatedLog.ts
+++ src/shared/lib/logger/purgeOutdatedLog.ts
@@ -1,6 +1,8 @@
 import isValidDirectoryPath from "@/shared/lib/validators/isValidDirectoryPath";
 import { readdir, stat, unlink } from "fs/promises";
 import path from "path";
+import formatMessage from "../messages/formatMessage";
+import loggerMessages from "assets/messages/loggerMessages.json";
 
 export default async function purgeOutdatedLog(
   targetPath: string
@@ -8,12 +10,16 @@ export default async function purgeOutdatedLog(
   const maxLogCnt = Number(process.env.MAX_LOG_CNT);
 
   if (!maxLogCnt || maxLogCnt <= 0) {
-    console.error(`MAX_LOG_CNT is not defined or invalid`);
+    console.error(formatMessage);
     return;
   }
 
   if (!(await isValidDirectoryPath(targetPath))) {
-    console.log(`Directory ${targetPath} does not exist`);
+    console.log(
+      formatMessage(loggerMessages.info.file.directory_does_not_exist, {
+        targetPath,
+      })
+    );
     return;
   }
 
@@ -36,13 +42,17 @@ export default async function purgeOutdatedLog(
         await unlink(filePath);
       }
     } else {
-      console.log(`Files count: ${files.length}`);
+      console.log(
+        formatMessage(loggerMessages.info.file.cnt, {
+          cnt: files.length.toString(),
+        })
+      );
     }
   } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.error(`Error deleting files: ${err.message}`);
-    } else {
-      console.error(`Unknown error: ${err}`);
-    }
+    console.error(
+      formatMessage(loggerMessages.error.file.delete, {
+        reason: err instanceof Error ? err.message : String(err),
+      })
+    );
   }
 }
