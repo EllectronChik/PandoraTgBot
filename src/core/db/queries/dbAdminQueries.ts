eb634175c4deb5ea660837dddcc2f7b58d6651aa
ellectronchik <electronchik04@mail.ru>
2024-12-23 15:24:20 +0300

    Update user creation query to use external ID
    - Modify the `createAdminUser` query to use `external_id` instead of `user_id` for user creation.
    - This change aligns with the database schema update to use external IDs for users.

--- src/core/db/queries/dbAdminQueries.ts
+++ src/core/db/queries/dbAdminQueries.ts
@@ -4,12 +4,12 @@ interface IQuery {
 
 const dbAdminQueries: IQuery = {
   createAdminUser: `
-    INSERT INTO users (user_id, admin, subscription_level, status, tag_id)
+    INSERT INTO users (external_id, admin, subscription_level, status, tag_id)
     VALUES (?, 1, NULL, 0, NULL);
   `,
   createAdminTrustToken: `
       INSERT INTO trust_tokens (user_id, token_hash)
-      VALUES ((SELECT id FROM users WHERE user_id = ?), ?);
+      VALUES ((SELECT id FROM users WHERE external_id = ?), ?);
   `,
 };
 
