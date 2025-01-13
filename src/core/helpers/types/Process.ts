48be9702e3cc892e470cf2684068d7fe0bab4670
ellectronchik <electronchik04@mail.ru>
2025-01-13 15:12:37 +0300

    Update bot type to include context in Process interface
    - Changed the type of the bot parameter in the Process interface from Bot to Bot<BotContext>.
    - This change ensures that the bot object has access to the context object, which provides additional information and functionality.

--- src/core/helpers/types/Process.ts
+++ src/core/helpers/types/Process.ts
@@ -1,9 +1,10 @@
 import { Logger } from "bs-logger";
 import { WriteStream } from "fs";
 import { Bot } from "grammy";
+import BotContext from "./BotContext";
 
 export default interface IProcess {
-  bot: Bot;
+  bot: Bot<BotContext>;
   errorStream: WriteStream;
   infoStream: WriteStream;
   logger: Logger;
