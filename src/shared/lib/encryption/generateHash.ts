c5ab88e8c44b934a6d212043e319d0aa69096db7
ellectronchik <electronchik04@mail.ru>
2025-01-13 15:20:24 +0300

    Refactor generateHash function to improve error handling
    - Removed the `min_length` parameter from the `generateHash` function to simplify error handling.
    - Updated the error message for `unhashedString` to make it more specific.
    - Ensured that `process.env.SECRET` is defined before generating the hash.

--- src/shared/lib/encryption/generateHash.ts
+++ src/shared/lib/encryption/generateHash.ts
@@ -1,14 +1,11 @@
 import { createHmac } from "crypto";
 
-export default function generateHash(
-  unhashedString: string,
-  min_length: number
-) {
+export default function generateHash(unhashedString: string) {
   if (!process.env.SECRET) {
     throw new Error("SECRET is not defined");
   }
-  if (!unhashedString || unhashedString.length <= min_length) {
-    throw new Error("unhashedString is too short");
+  if (!unhashedString) {
+    throw new Error("unhashedString is not defined");
   }
   return createHmac("sha256", process.env.SECRET)
     .update(unhashedString)
